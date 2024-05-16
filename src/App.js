1328c92a7e93597ba795b37f0ed334dda251bd76
Sehaj Wadhwa <104042287+sehajwadhwa@users.noreply.github.com>
2024-05-16 15:17:26 -0400

    styles setup

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
 import "../src/styles/global.scss";
 import Banner from "./components/Banner/Banner";
+import feedback from "./components/feedback/feedback";
+
 function App() {
   return (
     <BrowserRouter>
@@ -13,6 +15,7 @@ function App() {
         <Hero />
       </div>
       <Banner />
+      <feedback />
     </BrowserRouter>
   );
 }
