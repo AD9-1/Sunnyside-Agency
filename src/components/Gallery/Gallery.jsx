3f35673bc5cacb33752354234ae3d6761ddf5487
Sehaj Wadhwa <104042287+sehajwadhwa@users.noreply.github.com>
2024-05-16 18:09:56 -0400

    changes

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -1,22 +1,31 @@
-import React from 'react'
+import React from "react";
 import "./Gallery.scss";
 import Imagebottles from "../../assets/desktop/image-gallery-milkbottles.jpg";
 import Imageorange from "../../assets/desktop/image-gallery-orange.jpg";
 import Imagecone from "../../assets/desktop/image-gallery-cone.jpg";
 import Imagesugarcubes from "../../assets/desktop/image-gallery-sugarcubes.jpg";
 
-
 const Gallery = () => {
   return (
-    <div className='Gallery'>
-        
-          <img className='Gallery__images'src= {Imagebottles} alt='milkbottles' />
-          <img className='Gallery__images'src= {Imageorange} alt='oranges' />
-          <img className='Gallery__images'src= {Imagecone} alt='icecream cone' />
-          <img className='Gallery__images'src= {Imagesugarcubes} alt='sugarcubes' />
-        
+    <div className="Gallery">
+      <div>
+        <img className="Gallery__images" src={Imagebottles} alt="milkbottles" />
+      </div>
+      <div>
+        <img className="Gallery__images" src={Imageorange} alt="oranges" />
+      </div>
+      <div>
+        <img className="Gallery__images" src={Imagecone} alt="icecream cone" />
+      </div>
+      <div>
+        <img
+          className="Gallery__images"
+          src={Imagesugarcubes}
+          alt="sugarcubes"
+        />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Gallery
+export default Gallery;
