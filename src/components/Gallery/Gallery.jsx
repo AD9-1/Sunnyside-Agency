5320634ab8a42fcbf478767df035cd42560deb6c
Aparna Dhara <84341093+AD9-1@users.noreply.github.com>
2024-05-16 17:16:12 -0700

    merged

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -7,15 +7,6 @@ import Imagesugarcubes from "../../assets/desktop/image-gallery-sugarcubes.jpg";
 
 const Gallery = () => {
   return (
-<<<<<<< HEAD
-    <div className='Gallery'>
-        
-        <div> <img className='Gallery__images'src= {Imagebottles} alt='milkbottles' /></div>
-        <div><img className='Gallery__images'src= {Imageorange} alt='oranges' /></div>  
-         <div><img className='Gallery__images'src= {Imagecone} alt='icecream cone' /></div> 
-         <div><img className='Gallery__images'src= {Imagesugarcubes} alt='sugarcubes' /></div> 
-        
-=======
     <div className="Gallery">
       <div>
         <img className="Gallery__images" src={Imagebottles} alt="milkbottles" />
@@ -33,7 +24,6 @@ const Gallery = () => {
           alt="sugarcubes"
         />
       </div>
->>>>>>> 3f35673bc5cacb33752354234ae3d6761ddf5487
     </div>
   );
 };
